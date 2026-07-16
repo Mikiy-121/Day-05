4b739d3492936375db6c90816cdf9d4abc777b3a
Mikiyas teshale <mikiy256@gmail.com>
2026-07-16 10:50:30 +0300

    duck typing

---
+++ DuckTyping.py
@@ -0,0 +1,5 @@
+def show_balance(item):
+    print(item.balance) # just needs .balance
+show_balance(savings) # works
+show_balance(current) # works
+show_balance(wallet) # works, if it has .balance
