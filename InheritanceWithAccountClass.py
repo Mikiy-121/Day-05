f34fea9bd4400ef92c36c12cde94bfcf5a9e3db3
Mikiyas teshale <mikiy256@gmail.com>
2026-07-16 10:43:35 +0300

    inherit with account

---
+++ InheritanceWithAccountClass.py
@@ -0,0 +1,8 @@
+class SavingsAccount(Account):
+    def __init__(self, owner, number, balance=0, rate=0.05):
+        super().__init__(owner, number, balance)
+        self.rate = rate
+
+    def add_interest(self):
+        interest = self.balance * self.rate
+self.deposit(interest) # reuse parent method
